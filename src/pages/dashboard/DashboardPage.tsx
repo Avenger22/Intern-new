091c4bc65264732a35d1dc1b641d63b50fc967ae
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-04-27 17:17:09 +0200

    now working on dashboard

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,10 +1,11 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import onLogout from "../../main/store/stores/user/login.store.on-logout"
 import { useDispatch, useSelector } from "react-redux"
 import {RootState} from '../../main/store/redux/rootState'
 import { Link, useNavigate } from "react-router-dom";
 import HeaderCommon from "../../main/components/Common/HeaderCommon/HeaderCommon"
 import FooterCommon from "../../main/components/Common/FooterCommon/FooterCommon"
+import axios from "axios";
 
 const DashboardPage : FC = ()=>{
 
@@ -13,20 +14,46 @@ const DashboardPage : FC = ()=>{
     const dispatch = useDispatch();
     // #endregion
 
+    async function getProductsFromServer() {
+        let result = await (await axios.get(`/product/get-all?PageNumber=1&PageSize=20`)).data;
+        // console.log(result.data) save them in redux state
+    }
+
+    useEffect(()=> {
+        getProductsFromServer()
+    }, [])
+
     return (
 
         <>
 
             <HeaderCommon />
 
-            <button onClick = { function (e: any) {
-                e.stopPropagation()
-                dispatch(onLogout())
-            }}>
+            <div className="dashboard-wrapper">
 
-                Log Out
+                <button onClick = { function (e: any) {
+                    e.stopPropagation()
+                    dispatch(onLogout())
+                }}>
 
-            </button>
+                    Log Out
+
+                </button>
+
+                <div className="products-wrapper">
+
+                    {
+
+                    }
+                    <div className="product-item">
+
+                        
+
+                    </div>
+
+                </div>
+
+            </div>
                 
             <FooterCommon />
 
